36d2ad5a72114a31daf4482c99c330ff93352957
MarcisKraukis1 <36454562+MarcisKraukis1@users.noreply.github.com>
2022-05-10 21:28:53 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sunaVecums = 3;
+        String dogName = "Mailo";
+        double svarsSuns = 15.8;
+        float augumsSuns = 60.2F;
+        boolean irIzsalcis = true;
+        char dzimums = 'V';
+        char dzimumsDivi = 'S';
+
+        System.out.println("Mans suns");
+        System.out.println("Vecums");
+        System.out.println(sunaVecums);
+        System.out.println("Vārds");
+        System.out.println(dogName);
+        System.out.println("Svars");
+        System.out.println(svarsSuns + " Kg");
+        System.out.println("Mans suņa augstums: " +  augumsSuns + " cm");
+        System.out.println(augumsSuns);
+        System.out.println("Vai suns ir izsalcis?");
+        System.out.println(irIzsalcis);
+
+
+        int gadaApgrozijumsDolaros = 2147483647;
+        byte monthsInYear = 12;
+        short autoPrice = 12312;
+        long netIncome = 2147483699L;
+        System.out.println(gadaApgrozijumsDolaros + "\n" + monthsInYear + "\n" + autoPrice + "\n" + netIncome);
+
+
+       /* String kakjaVards = "Garfilds";
+        int kakjaVecums= 7;
+
+        System.out.println("Mans kaķis");
+        System.out.println("Vecums");
+        System.out.println(kakjaVecums);
+        System.out.println("Vārds");
+        System.out.println(kakjaVards);
+
+        System.out.println("Kaķa un suņa kopējais vecums ir: ");
+        System.out.println(sunaVecums + kakjaVecums);
+        System.out.println("Hello Marcis");
+
+        */
+
+        int atlikums;
+        int summa;
+        double dalijums;
+        double z = 5;
+        double q =10;
+        atlikums = 13 % 5;
+        int x = 5;
+        int y = 10;
+        summa = x + y;
+        dalijums =z / q;
+        System.out.println("Dalījums " + dalijums);
+        System.out.println(summa);
+        System.out.println(atlikums);
+    }
+}
